f056af605533af9cec32f47e8a2eb085ad7570b7
Anyanwu Chukwuemeka <chrisemeka096@gmail.com>
2025-11-14 15:33:33 +0100

    added: the cors implemenation in the index.ts for the backend and .env to the .gitignore for the frontend folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Response, Request, NextFunction } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoute from "./routes/auth";
+import sceneRoute from "./routes/scenes";
 
 
 dotenv.config();
@@ -8,8 +10,14 @@ const app: Express = express();
 
 const port = process.env.PORT!;
 
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
+}));
+
 app.use(express.json());
 app.use('/auth', authRoute);
+app.use('/api/scenes', sceneRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
